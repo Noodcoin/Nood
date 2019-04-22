a1102d8f37e88183e3c5949e7a5430bd0fc14d2e
Noodcoin <49656488+Noodcoin@users.noreply.github.com>
2019-04-21 20:10:36 -0500

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -142,8 +142,8 @@ public:
 	
 	
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 53);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 54);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 63);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 64);
         base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 73);
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0xAD)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x1E)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
